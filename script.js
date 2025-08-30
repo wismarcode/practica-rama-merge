fe9e587d7e370f86c416bb5e03ac7a399cbfe516
Wismar Ruiz <142424264+Wismarruiz@users.noreply.github.com>
2025-08-30 11:55:50 -0400

    Otra forma de eliminar le último carácter

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
 
-  function deshacer() {
-    const valorPantalla = document.getElementById('pantalla').value;
-    const resultado = valorPantalla.substring(0, valorPantalla.length -1)
-    document.getElementById("pantalla").value = resultado
+function deshacer() {
+    const valorPantalla = document.getElementById("pantalla").value;
+    document.getElementById("pantalla").value = valorPantalla.slice(0,-1)
   }
